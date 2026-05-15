b77490fb9c0ec3182f1b0543367f14541637a711
John J Arenas N <jarenas@jgs.com.co>
2026-05-15 16:26:50 -0500

    Ajustar configuracion por ambientes y SMTP staging

--- Services/Notificaciones/Config/EmailService.cs
+++ Services/Notificaciones/Config/EmailService.cs
@@ -1,4 +1,5 @@
 ﻿using Alertas.Services.Notificaciones.Config;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using System.Net;
 using System.Net.Mail;
@@ -8,10 +9,14 @@ namespace Alertas.Services.Notificaciones
     public class EmailService : IEmailService
     {
         private readonly SmtpSettings _settings;
+        private readonly IWebHostEnvironment _env;
 
-        public EmailService(IOptions<SmtpSettings> options)
+        public EmailService(
+            IOptions<SmtpSettings> options,
+            ILogger<EmailService> logger)
         {
             _settings = options.Value;
+            _logger = logger;
         }
 
         public async Task EnviarEmailAsync(
@@ -19,13 +24,34 @@ namespace Alertas.Services.Notificaciones
             string asunto,
             string htmlBody)
         {
+            _logger.LogInformation(
+            "SMTP RedirectAllTo: {RedirectAllTo}. Destinatario original: {Destinatario}",
+            _settings.RedirectAllTo,
+            destinatario);
+
+            var destinatarioFinal = destinatario;
+
+            if (!string.IsNullOrWhiteSpace(_settings.RedirectAllTo))
+            {
+                asunto = $"[STAGING - REDIRIGIDO] {asunto}";
+                htmlBody = $@"
+                <div style='padding:10px;background:#fff3cd;border:1px solid #ffeeba;margin-bottom:15px;'>
+                    <strong>Correo redirigido desde ambiente de pruebas.</strong><br/>
+                    Destinatario original: {destinatario}
+                </div>
+                {htmlBody}";
+
+                destinatario = _settings.RedirectAllTo;
+            }
+
             using var message = new MailMessage();
 
             message.From = new MailAddress(
                 _settings.FromEmail,
                 _settings.FromName);
 
-            message.To.Add(destinatario);
+            message.To.Add(destinatarioFinal);
+
             message.Subject = asunto;
             message.Body = htmlBody;
             message.IsBodyHtml = true;
@@ -40,5 +66,8 @@ namespace Alertas.Services.Notificaciones
 
             await smtp.SendMailAsync(message);
         }
+
+        private readonly ILogger<EmailService> _logger;
+
     }
 }
